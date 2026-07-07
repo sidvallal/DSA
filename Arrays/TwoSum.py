3477a48d264e2893c12519b9f6df677f5be6de58
Siddharth Vallal <siddharthvallal@gmail.com>
2026-07-07 17:03:45 +0530

    Optimizied

--- Arrays/TwoSum.py
+++ Arrays/TwoSum.py
@@ -4,12 +4,22 @@ arr = np.array([2,4,2,1,3,5,1])
 
 target = 7
 
-for i in range(len(arr)):
-    for j in range(i+1,len(arr)):
-        if arr[i] + arr[j] == target:
-            print('yes')
-            print(arr[i], arr[j])
-            break
+# for i in range(len(arr)):
+#     for j in range(i+1,len(arr)):
+#         if arr[i] + arr[j] == target:
+#             print('yes')
+#             print(arr[i], arr[j])
+#             break
+
+seen = {}
+
+for i,num in enumerate(arr):
+    comp = target - num
+    
+    if comp in seen :
+        print("Indices : ",seen[comp],i)
+        break
+    seen[num] = i
 
 
             
