320ceaf95f872014601bc8c4dd5a27db1a224502
Siddharth Vallal <siddharthvallal@gmail.com>
2026-07-07 17:00:03 +0530

    brute force

---
+++ Arrays/TwoSum.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+arr = np.array([2,4,2,1,3,5,1])
+
+target = 7
+
+for i in range(len(arr)):
+    for j in range(i+1,len(arr)):
+        if arr[i] + arr[j] == target:
+            print('yes')
+            print(arr[i], arr[j])
+            break
+
+
+            
+
+        
