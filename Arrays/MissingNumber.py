339b802c1a488100fa745b1b17a254c42c0ed60e
Siddharth Vallal <siddharthvallal@gmail.com>
2026-07-08 20:39:55 +0530

    Better approach - >  MissingNumber Problem

--- Arrays/MissingNumber.py
+++ Arrays/MissingNumber.py
@@ -1,6 +1,12 @@
 arr = [0,1,2,3,5]
 
-for i in range(len(arr)):
-    if i not in arr:
+# for i in range(len(arr)):
+#     if i not in arr:
+#         print(i)
+#         break
+
+s = set(arr)
+for i in range(len(arr)+1):
+    if i not in s:
         print(i)
         break
