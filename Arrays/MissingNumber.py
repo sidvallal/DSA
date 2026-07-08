6b68f5454597a2ee7dcbb3a11549683e3cf14bbe
Siddharth Vallal <siddharthvallal@gmail.com>
2026-07-08 20:45:02 +0530

    Optimal using XOR - >  MissingNumber Problem

--- Arrays/MissingNumber.py
+++ Arrays/MissingNumber.py
@@ -5,8 +5,15 @@ arr = [0,1,2,3,5]
 #         print(i)
 #         break
 
-s = set(arr)
-for i in range(len(arr)+1):
-    if i not in s:
-        print(i)
-        break
+# s = set(arr)
+# for i in range(len(arr)+1):
+#     if i not in s:
+#         print(i)
+#         break
+
+missing = len(arr)
+for i in range(len(arr)):
+    missing ^=i
+    missing ^= arr[i]
+    
+print(missing)
