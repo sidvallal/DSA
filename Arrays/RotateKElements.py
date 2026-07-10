713e9cff69fc21349d14703026735ab388a63f3b
Siddharth Vallal <siddharthvallal@gmail.com>
2026-07-10 10:32:13 +0530

    Rotate k elements

---
+++ Arrays/RotateKElements.py
@@ -0,0 +1,19 @@
+def rotate(arr,k):
+    k %= len(arr)
+    
+    def reverse(left,right):
+        while left < right:
+            
+            arr[left],arr[right] = arr[right],arr[left]
+            left+=1
+            right-=1
+            
+    reverse(0,len(arr)-1)
+    reverse(0,k-1)
+    reverse(k,len(arr)-1)
+
+arr = [1,2,3,4,5,6,7]
+rotate(arr,3)
+print(arr)   
+
+        
