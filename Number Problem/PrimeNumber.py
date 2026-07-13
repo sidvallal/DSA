fbaa36dbc296abfe12d96cbabc492207029f342b
Siddharth Vallal <siddharthvallal@gmail.com>
2026-07-13 23:03:46 +0530

    Prime Number

--- Number Problem/PrimeNumber.py
+++ Number Problem/PrimeNumber.py
@@ -1,11 +1,26 @@
-for num in range(2, 101):
+# for num in range(2, 101):
 
-    is_prime = True
+#     is_prime = True
 
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
+#     for i in range(2, int(num ** 0.5) + 1):
+#         if num % i == 0:
+#             is_prime = False
+#             break
+
+#     if is_prime:
+#         print(num, end=" ")
+n = 17
+
+is_prime = True
+
+if n <= 1:
+    is_prime = False
+else:
+    i = 2
+    while i * i <= n:
+        if n % i == 0:
             is_prime = False
             break
+        i += 1
 
-    if is_prime:
-        print(num, end=" ")
+print(is_prime)
