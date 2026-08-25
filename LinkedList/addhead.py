214e3eb3613e9e6bab73adf151144efa5cb7a19e
Siddharth Vallal <siddharthvallal@gmail.com>
2026-08-25 10:40:53 +0530

    insert at position and insert before specific value

--- LinkedList/addhead.py
+++ LinkedList/addhead.py
@@ -29,15 +29,19 @@ class linkedList:
             print(current.data, end=" ")
             current = current.next
 
-    def arr2list(self,numbers: list):
-        head = Node(numbers[0])
-        mover  = head
-        for i in range(1,len(numbers)):
+    def arr2list(self, numbers: list):
+        if len(numbers) == 0:
+            return None
+
+        self.head = Node(numbers[0])
+        mover = self.head
+
+        for i in range(1, len(numbers)):
             temp = Node(numbers[i])
             mover.next = temp
             mover = temp
 
-        return head
+        return self.head
 
     def traval(self,head):
         current = head
@@ -46,63 +50,86 @@ class linkedList:
             current = current.next
 
     def addhead(self,k):
-        if head == None: 
-            return head
+        if self.head == None: 
+            temp = Node(k)
+            return temp
         temp = Node(k)
-        temp.next = head
+        temp.next = self.head
         return temp
 
-        return head.data
-    def removetail(self,head):
-        if head == None:
-            return head
-        if head.next == None:
-            return head
+    def addtail(self,k):
+        if self.head == None:
+            self.head = Node(k)
+            return self.head
         
-        current = head
+        temp = self.head
 
-        while current.next.next is not None:
-            current = current.next
-
-        current.next = None
-        return head
-
-    def deleteK(self,head,k):
-        if head == None:
-            return head
-        if head.next == None:
-            temp = head
-            head = head.next
-            return head
-        cnt = 0
-        temp = head
-        prev = None
-
-        while temp is not None:
-            cnt +=1
-            if cnt == k:
-                prev.next = prev.next.next
-                break
-            prev = temp
+        while temp.next is not None:
             temp = temp.next
-        return head
+
+        new_node = Node(k)
+        temp.next = new_node
+
+        return self.head
+        
+
+    def addKth(self, k, data):
+        new_node = Node(data)
+
+        # Empty list
+        if self.head is None:
+            if k == 1:
+                self.head = new_node
+            return self.head
+
+        # Insert at position 1
+        if k == 1:
+            new_node.next = self.head
+            self.head = new_node
+            return self.head
+
+        temp = self.head
+        count = 1
+
+        # Move to the node just before position k
+        while temp is not None and count < k - 1:
+            temp = temp.next
+            count += 1
+
+        # k is greater than list length + 1
+        if temp is None:
+            return self.head
+
+        new_node.next = temp.next
+        temp.next = new_node
+
+        return self.head
     
-    def deleteElement(self,head,el):
-            if head == None:
-                return head
-            if head.data == el :
-                temp = head
-                head = head.next
-                return head
-            temp = head
-            prev = None
-            while temp is not None:
-                if temp.data == el:
-                    prev.next = prev.next.next
-                    break
-                prev = temp
-                temp = temp.next
-            return head
+    def insertBeforeX(self, data, x):
+        new_node = Node(data)
+
+        # Empty list
+        if self.head is None:
+            return self.head
+
+        # x is at the head
+        if self.head.data == x:
+            new_node.next = self.head
+            self.head = new_node
+            return self.head
+
+        temp = self.head
+
+        while temp.next is not None:
+            if temp.next.data == x:
+                new_node.next = temp.next
+                temp.next = new_node
+                return self.head
+
+            temp = temp.next
+
+        # x not found
+        return self.head
         
     
 arr = [1,2,3,4,5]
@@ -117,7 +144,13 @@ head = mylist.arr2list(arr)
 # mylist.traval(ans)
 # ans = mylist.deleteElement(head,4)
 # mylist.traval(ans)
-ans = mylist.addhead(0)
+# ans = mylist.addhead(0)
+# mylist.traval(ans)
+# ans = mylist.addtail(6)
+# mylist.traval(ans)
+# ans = mylist.addKth(3,3)
+# mylist.traval(ans)
+ans = mylist.insertBeforeX(10,2)
 mylist.traval(ans)
 
 
