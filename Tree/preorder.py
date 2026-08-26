d07d480b4a5c82fa4727d44b0c7e96b60fabb39d
Siddharth Vallal <siddharthvallal@gmail.com>
2026-08-26 18:38:59 +0530

    iterative preorder

--- Tree/preorder.py
+++ Tree/preorder.py
@@ -13,6 +13,27 @@ def preorder(node):
     preorder(node.left)
     preorder(node.right)
 
+def iterativepreorder(node):
+    if node is None:
+        return None
+
+    stack = [root]
+    result = []
+
+    while stack:
+        node = stack.pop()
+
+        result.append(node.data)
+
+        if node.right:
+            stack.append(node.right)
+
+        if node.left:
+            stack.append(node.left)
+    return result
+
+
+
 root = Node(10)
 root.left = Node(5)
 root.right = Node(7)
@@ -20,3 +41,5 @@ root.left.left = Node(6)
 root.left.right = Node(2)
 
 preorder(root)
+ans = iterativepreorder(root)
+print(ans)
