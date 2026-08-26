66901bd918d08bf5ded283f8c0ae1becc82b234c
Siddharth Vallal <siddharthvallal@gmail.com>
2026-08-26 19:08:32 +0530

    Iterative Postorder Travalsal

--- Tree/postorder.py
+++ Tree/postorder.py
@@ -13,6 +13,30 @@ def postorder(node):
     postorder(node.right)
     print(node.data)
 
+def postorderTraversal(root):
+    if root is None:
+        return []
+
+    stack1 = [root]
+    stack2 = []
+    result = []
+
+    while stack1:
+        current = stack1.pop()
+        stack2.append(current)
+
+        if current.left:
+            stack1.append(current.left)
+
+        if current.right:
+            stack1.append(current.right)
+
+    while stack2:
+        current = stack2.pop()
+        result.append(current.data)
+
+    return result
+
 root = Node(10)
 root.left = Node(5)
 root.right = Node(7)
@@ -20,3 +44,6 @@ root.left.left = Node(6)
 root.left.right = Node(2)
 
 postorder(root)
+ans = postorderTraversal(root)
+
+print(ans)
