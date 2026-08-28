279a98f411413f3e3d65190c0e2c7dc2441bdfbb
Siddharth Vallal <siddharthvallal@gmail.com>
2026-08-28 14:51:20 +0530

    implemented basic backtracking recurssion

--- Recurssion/recurssion.py
+++ Recurssion/recurssion.py
@@ -1,21 +1,27 @@
 cnt = 0
 
 def fun():
-
     global cnt
-
     if cnt == 5:
         return
     print(cnt, end=" ")
     cnt += 1
-
     fun()
 
 def name(i,n):
-    
+
     if i > n:
         return
     print('sid')
     name(i+1,n)
 
-name(1,5)
+def backtrack(i,n):
+    if i<1:
+        return
+
+    backtrack(i-1,n)
+
+    print(i)
+
+# name(1,5)
+backtrack(5,5)
